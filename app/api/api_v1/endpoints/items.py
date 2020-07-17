668824b78a0de737cc6f13f280729793edfd6f46
Srdjan Stankovic <stankovic.srdjo@gmail.com>
2020-07-17 12:55:04 +0200

    Update SAM template with API Gateway

--- app/api/api_v1/endpoints/items.py
+++ app/api/api_v1/endpoints/items.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from app.schemas import item
+
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_model=item.ItemBase)
 async def read_items():
-    return "items"
+    return {"title": "Test", "description": "Test"}
